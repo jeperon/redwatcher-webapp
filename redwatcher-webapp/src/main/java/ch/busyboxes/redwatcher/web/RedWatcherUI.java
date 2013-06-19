2846e4ba2586b8076bd3e9e49088180a5c5d384d
Julien Eperon <julien.eperon@gmail.com>
2013-06-19 15:38:57 +0200

    Upgrading to Vaadin 7.0.7

---
+++ redwatcher-webapp/src/main/java/ch/busyboxes/redwatcher/web/RedWatcherUI.java
@@ -0,0 +1,33 @@
+package ch.busyboxes.redwatcher.web;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+public class RedWatcherUI extends UI {
+
+	/**
+	 * Serial version UID
+	 */
+	private static final long serialVersionUID = -7193721015839507497L;
+
+	@Override
+	protected void init(VaadinRequest request) {
+		final VerticalLayout layout = new VerticalLayout();
+		layout.setMargin(true);
+		setContent(layout);
+
+		Button button = new Button("Click Me");
+		button.addClickListener(new Button.ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				layout.addComponent(new Label("Thank you for clicking"));
+			}
+		});
+		layout.addComponent(button);
+
+	}
+
+}
